53e70b90b7f4764b321beced78a65780131ca6bc
liamod <liamgregoryod@gmail.com>
2025-06-20 16:51:24 +0200

    Basic tmux setup

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -1,25 +1,25 @@
 -- WezTerm Configuration
 
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.initial_cols = 120
 config.initial_rows = 60
 
 config.font_size = 12
-config.font = wezterm.font('Hack Nerd Font')
+config.font = wezterm.font("Hack Nerd Font")
 -- config.font = wezterm.font('JetBrains Mono')
 -- config.font = wezterm.font('Monaspace Neon Frozen')
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Macchiato"
 config.window_background_opacity = 1.0
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 1,
-  right = 1,
-  top = 1,
-  bottom = 1,
+	left = 1,
+	right = 1,
+	top = 1,
+	bottom = 1,
 }
 
 config.audible_bell = "Disabled"
